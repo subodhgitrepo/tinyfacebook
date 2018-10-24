fc2f9c49ef2d30196e06e48c58e9fe19bfd83eeb
Subodh Kalika <kalikasubodh@gmail.com>
2018-10-24 10:51:51 +0545

    posting

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,7 +4,7 @@
 	<div class="text-center">
 		<h1>WELCOME TO TINYFACEBOOK</h1>
 		@guest
-			<h3><a href="/login">Login</a> or <a href="/register">Register</a> to start sposting</h3>
+			<h3><a href="/login">Login</a> or <a href="/register">Register</a> to start posting</h3>
 		@else
 			Start Posting. Go to your <a href="/profile">Profile</a>
 		@endguest
